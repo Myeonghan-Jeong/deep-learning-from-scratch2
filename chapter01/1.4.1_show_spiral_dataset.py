37e096ff5708e160229fc86eb409481dc1e4ed36
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-02-23 16:48:15 +0900

    study 1.4.1_show_spiral_dataset.py

---
+++ chapter01/1.4.1_show_spiral_dataset.py
@@ -0,0 +1,15 @@
+from datasets import spiral
+import matplotlib.pyplot as plt
+
+x, t = spiral.load_data()
+print('x', x.shape)  # (300, 2)
+print('t', t.shape)  # (300, 3), one-hot encoded
+
+# plot datasets
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i * N:(i + 1) * N, 0],
+                x[i * N:(i + 1) * N, 1], s=40, marker=markers[i])
+plt.show()

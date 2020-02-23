01e8da3fd9f362ac01fc60cee2b61d3eb4ab3864
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-02-23 16:48:44 +0900

    study 1.4.3_train_custom_loop.py

--- chapter01/1.4.3_train_custom_loop.py
+++ chapter01/1.4.3_train_custom_loop.py
@@ -1,25 +1,21 @@
-# coding: utf-8
-import sys
-sys.path.append('..')  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
-import numpy as np
-from common.optimizer import SGD
-from dataset import spiral
-import matplotlib.pyplot as plt
 from two_layer_net import TwoLayerNet
+from commons.optimizer import SGD
+from datasets import spiral
+import matplotlib.pyplot as plt
+import numpy as np
 
-
-# 하이퍼파라미터 설정
+# set hyperparameters
 max_epoch = 300
 batch_size = 30
 hidden_size = 10
 learning_rate = 1.0
 
-# 데이터 읽기, 모델과 옵티마이저 생성
+# read data and set model, optimizer
 x, t = spiral.load_data()
 model = TwoLayerNet(input_size=2, hidden_size=hidden_size, output_size=3)
 optimizer = SGD(lr=learning_rate)
 
-# 학습에 사용하는 변수
+# variables for learning
 data_size = len(x)
 max_iters = data_size // batch_size
 total_loss = 0
@@ -27,7 +23,7 @@ loss_count = 0
 loss_list = []
 
 for epoch in range(max_epoch):
-    # 데이터 뒤섞기
+    # suffle datas for mini batch
     idx = np.random.permutation(data_size)
     x = x[idx]
     t = t[idx]
@@ -36,7 +32,7 @@ for epoch in range(max_epoch):
         batch_x = x[iters*batch_size:(iters+1)*batch_size]
         batch_t = t[iters*batch_size:(iters+1)*batch_size]
 
-        # 기울기를 구해 매개변수 갱신
+        # calculate grad and renewal params
         loss = model.forward(batch_x, batch_t)
         model.backward()
         optimizer.update(model.params, model.grads)
@@ -44,22 +40,22 @@ for epoch in range(max_epoch):
         total_loss += loss
         loss_count += 1
 
-        # 정기적으로 학습 경과 출력
+        # return learning information
         if (iters+1) % 10 == 0:
             avg_loss = total_loss / loss_count
-            print('| 에폭 %d |  반복 %d / %d | 손실 %.2f'
+            print('| Epoch %d |  Loop %d / %d | Loss %.2f'
                   % (epoch + 1, iters + 1, max_iters, avg_loss))
             loss_list.append(avg_loss)
             total_loss, loss_count = 0, 0
 
 
-# 학습 결과 플롯
+# plot results
 plt.plot(np.arange(len(loss_list)), loss_list, label='train')
-plt.xlabel('반복 (x10)')
-plt.ylabel('손실')
+plt.xlabel('loop (x10)')
+plt.ylabel('loss')
 plt.show()
 
-# 경계 영역 플롯
+# plot barriers
 h = 0.001
 x_min, x_max = x[:, 0].min() - .1, x[:, 0].max() + .1
 y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
@@ -71,11 +67,12 @@ Z = predict_cls.reshape(xx.shape)
 plt.contourf(xx, yy, Z)
 plt.axis('off')
 
-# 데이터점 플롯
+# plot data points
 x, t = spiral.load_data()
 N = 100
 CLS_NUM = 3
 markers = ['o', 'x', '^']
 for i in range(CLS_NUM):
-    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+    plt.scatter(x[i * N:(i + 1) * N, 0],
+                x[i * N:(i + 1) * N, 1], s=40, marker=markers[i])
 plt.show()

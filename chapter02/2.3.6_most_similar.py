b1146297c5b6e33afc883f51a0c594ac59fe301a
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-03-17 14:14:28 +0900

    study 2.3.6_most_similar.py

--- chapter02/2.3.6_most_similar.py
+++ chapter02/2.3.6_most_similar.py
@@ -1,8 +1,4 @@
-# coding: utf-8
-import sys
-sys.path.append('..')
-from common.util import preprocess, create_co_matrix, most_similar
-
+from commons.util import preprocess, create_co_matrix, most_similar
 
 text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)

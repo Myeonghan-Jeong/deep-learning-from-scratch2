1afe3e9b3c4efc10188f0b47164281f40500b07b
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-03-20 20:32:28 +0900

    study 2.4.3_count_method_small.py

--- chapter02/2.4.3_count_method_small.py
+++ chapter02/2.4.3_count_method_small.py
@@ -1,10 +1,6 @@
-# coding: utf-8
-import sys
-sys.path.append('..')
-import numpy as np
+from commons.util import preprocess, create_co_matrix, ppmi
 import matplotlib.pyplot as plt
-from common.util import preprocess, create_co_matrix, ppmi
-
+import numpy as np
 
 text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
@@ -15,13 +11,12 @@ W = ppmi(C)
 # SVD
 U, S, V = np.linalg.svd(W)
 
-np.set_printoptions(precision=3)  # 유효 자릿수를 세 자리로 표시
+np.set_printoptions(precision=3)  # set precision number 3
 print(C[0])
 print(W[0])
 print(U[0])
 
-# 플롯
 for word, word_id in word_to_id.items():
     plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
-plt.scatter(U[:,0], U[:,1], alpha=0.5)
+plt.scatter(U[:, 0], U[:, 1], alpha=0.5)
 plt.show()

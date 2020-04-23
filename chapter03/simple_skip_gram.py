18a8ce6b89f1cce617aa8d3745c74c7d16a9e923
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-04-23 18:00:20 +0900

    update simple_skip_gram.py

--- chapter03/simple_skip_gram.py
+++ chapter03/simple_skip_gram.py
@@ -1,32 +1,29 @@
-# coding: utf-8
-import sys
-sys.path.append('..')
+from commons.layers import MatMul, SoftmaxWithLoss
 import numpy as np
-from common.layers import MatMul, SoftmaxWithLoss
 
 
 class SimpleSkipGram:
     def __init__(self, vocab_size, hidden_size):
         V, H = vocab_size, hidden_size
 
-        # 가중치 초기화
+        # init weights
         W_in = 0.01 * np.random.randn(V, H).astype('f')
         W_out = 0.01 * np.random.randn(H, V).astype('f')
 
-        # 계층 생성
+        # init layers
         self.in_layer = MatMul(W_in)
         self.out_layer = MatMul(W_out)
         self.loss_layer1 = SoftmaxWithLoss()
         self.loss_layer2 = SoftmaxWithLoss()
 
-        # 모든 가중치와 기울기를 리스트에 모은다.
+        # collect all params and grads
         layers = [self.in_layer, self.out_layer]
         self.params, self.grads = [], []
         for layer in layers:
             self.params += layer.params
             self.grads += layer.grads
 
-        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
+        # save word representation in instance variable
         self.word_vecs = W_in
 
     def forward(self, contexts, target):
